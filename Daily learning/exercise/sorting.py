8ce146c6881973e5fb58a808b2e7a6fd3606c72e
mutanara <mutabarukan@gmail.com>
2019-04-23 16:21:56 +0200

    23rd end

--- Daily learning/exercise/sorting.py
+++ Daily learning/exercise/sorting.py
@@ -1,32 +1,32 @@
 #First way of doing it at once.
 
 
-# list_fam = [
-#     "Nara","mahame","Gemma","gustave","Vicky",
-#     1995,1991,1989,1986,1985,19.95,19.91,19.89,19.86,19.85
-#     ]
+list_fam = [
+    "Nara","mahame","Gemma","gustave","Vicky",
+    1995,1991,1989,1986,1985,19.95,19.91,19.89,19.86,19.85
+    ]
 
-# list_names = []
-# list_bd = []
-# list_dec = []
+list_names = []
+list_bd = []
+list_dec = []
 
-# for i in list_fam:
-#     if isinstance(i,str):
-#         list_names.append(i)
-#     list_names.sort(key=str.lower)
-# print(list_names)
+for i in list_fam:
+    if isinstance(i,str):
+        list_names.append(i)
+    list_names.sort(key=str.lower)
+print(list_names)
 
-# for i in list_fam:
-#     if isinstance(i, int):
-#         list_bd.append(i)
-#     list_bd.sort(reverse = False)
-# print(list_bd)
+for i in list_fam:
+    if isinstance(i, int):
+        list_bd.append(i)
+    list_bd.sort(reverse = False)
+print(list_bd)
 
-# for i in list_fam:
-#     if isinstance(i, float):
-#         list_dec.append(i)
-#     list_dec.sort(reverse = False)
-# print(list_dec)
+for i in list_fam:
+    if isinstance(i, float):
+        list_dec.append(i)
+    list_dec.sort(reverse = False)
+print(list_dec)
 
 
 #Second way of doing it
--- Daily learning/exercise/zipfunction.py
+++ Daily learning/exercise/zipfunction.py
@@ -1,24 +1,20 @@
-source_names = ["Plante Niyomugenga", " Narcisse Mutabaruka", "Silas Gasasira", "Jean Luc Abayo"]
+source_names = ["fourth_ex", "second_ex", "first_ex", "third_ex"]
 
 source_ps = [ "I never considered you a nice guy.", " You are insane boo, you know that?",
  "I regret 2016, I should have been more nice with you!", "Be careful, you might only be good at programming."]
 
-# first_way to do it.
-
-# zip (source_names, source_ps)
-
-# for x in zip (source_names, source_ps):
-#     print(x)
+#First_way to do it:
+zip (source_names, source_ps)
+for x in zip (source_names, source_ps):
+    print(x)
 
 
-#second_way to do it: 
+#Second_way to do it: 
+for x, y in zip(source_names,source_ps):
+    print(x,y)
 
-# for x, y in zip(source_names,source_ps):
-#     print(x,y)
-
-# third_way to do it:
-
-# for x,y in zip(source_names, source_ps):
-#     zipped = (x,y)
-#     print (zipped)
+#Third_way to do it:
+for x,y in zip(source_names, source_ps):
+    zipped = (x,y)
+    print (zipped)
 
